1d72173e88ff6511712d2497ffdfb898a8367872
Markus Karileet <markuskarileet@hotmail.com>
2014-02-03 20:21:35 +0200

    trying another config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,11 +16,11 @@ TARGET_BOOTLOADER_BOARD_NAME := rk2928sdk #rkr70sdk
 BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.console=ttyFIQ0 init=/init initrd=0x62000000,0x00240000 mtdparts=rk29xxnand:0x00002000@0x00000000(parameter),0x00002000@0x00002000(misc),0x00004000@0x00004000(kernel),0x00008000@0x00008000(boot),0x00010000@0x00010000(recovery),0x00020000@0x00020000(backup),0x00040000@0x00040000(cache),0x00200000@0x00080000(userdata),0x00002000@0x00280000(kpanic),0x00100000@0x00282000(system),-@0x00382000(user) bootver=2012-12-25#1.24 firmware_ver=4.1.1
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000 #0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x02000000 #0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912 #0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824 #0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 4096 #131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x02000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824
+BOARD_FLASH_BLOCK_SIZE := 4096
 
 TARGET_PREBUILT_KERNEL := device/goclever/rkr70sdk/kernel
 
@@ -28,7 +28,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_PROVIDES_INIT := true
-TARGET_RECOVERY_INITRC := device/goclever/rkr70sdk/init.rc
+TARGET_RECOVERY_INITRC := device/goclever/rkr70sdk/recovery/init.rc
 BOARD_CUSTOM_BOOTIMG_MK := device/goclever/rkr70sdk/rkcrc_sign.mk
 
 TARGET_BOARD_PLATFORM_GPU := mali400
@@ -40,7 +40,7 @@ BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
 BOARD_UMS_2ND_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun1/file
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-#Bluethoot
+#Bluetooth
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_BLUETOOTH_CSR := false
 
@@ -57,7 +57,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/goclever/rkr70sdk/releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/goclever/rkr70sdk/releasetools/rk292x_ota_from_target_files
 
 # Graphics
-BOARD_EGL_CFG := device/goclever/rkr70sdk/egl.cfg
+BOARD_EGL_CFG := vendor/goclever/rkr70sdk/proprietary/system/lib/egl/egl.cfg
 USE_OPENGL_RENDERER := true
 TARGET_DISABLE_TRIPLE_BUFFERING := true
 ENABLE_WEBGL := true
@@ -72,23 +72,3 @@ COMMON_GLOBAL_CFLAGS += -DSURFACEFLINGER_FORCE_SCREEN_RELEASE -DNO_RGBX_8888 -DM
 
 # HWComposer
 BOARD_USES_HWCOMPOSER := true
-
-# Wifi stuff
-BOARD_WIFI_VENDOR := realtek
-
-#WPA_SUPPLICANT_VERSION := VER_0_8_X #not sure
-#BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
-
-BOARD_WLAN_DEVICE := rtl8188eu
-WIFI_DRIVER_MODULE_NAME   := wlan
-WIFI_DRIVER_MODULE_PATH   := "/system/lib/modules/wlan.ko"
-
-WIFI_DRIVER_MODULE_ARG    := ""
-WIFI_FIRMWARE_LOADER      := ""
-WIFI_DRIVER_FW_PATH_STA   := ""
-WIFI_DRIVER_FW_PATH_AP    := ""
-WIFI_DRIVER_FW_PATH_P2P   := ""
-WIFI_DRIVER_FW_PATH_PARAM := ""
-
-TARGET_CUSTOM_WIFI := ../../hardware/realtek/wlan/libhardware_legacy/wifi/wifi_realtek.c
