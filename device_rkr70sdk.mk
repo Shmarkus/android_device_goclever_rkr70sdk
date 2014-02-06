d23af7bef2f93f80c1156488ecbfacc0f6cbbd8f
Markus Karileet <markuskarileet@hotmail.com>
2014-02-06 19:58:00 +0200

    removed values that had no effect added features and platform permissions from 4.1.1

--- device_rkr70sdk.mk
+++ device_rkr70sdk.mk
@@ -7,6 +7,7 @@ $(call inherit-product-if-exists, vendor/goclever/rkr70sdk/rkr70sdk-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/goclever/rkr70sdk/overlay
 VENDOR_PATH := vendor/goclever/rkr70sdk
+
 LOCAL_PATH := device/goclever/rkr70sdk
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
@@ -14,7 +15,6 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-# Ramdisk
 PRODUCT_COPY_FILES += \
 	$(LOCAL_KERNEL):kernel \
 	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
@@ -28,11 +28,7 @@ $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-PRODUCT_TAGS += dalvik.gc.type-precise
-
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=120 \
-    ro.product.usbfactory=rockchip_usb \
-    sys.hwc.compose_policy=0 \
 	sf.power.control=2073600 \
 	ro.sf.fakerotation=true \
 	ro.sf.hwrotation=0 \
@@ -47,17 +43,21 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=120 \
 	dalvik.vm.dexopt-flags=m=y \
 	dalvik.vm.jniopts=warnonly \
     ro.opengles.version=131072 \
-    libc.debug.malloc=10
+    libc.debug.malloc=10 \
+    ro.product.usbfactory=rockchip_usb \
+    persist.sys.usb.config=mass_storage
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
+    $(VENDOR_PATH)/proprietary/system/etc/permissions/features.xml:system/etc/permissions/features.xml \
+    $(VENDOR_PATH)/proprietary/system/etc/permissions/platform.xml:system/etc/permissions/platform.xml
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_PACKAGES += \
@@ -71,35 +71,11 @@ PRODUCT_PACKAGES += \
 	VisualizationWallpapers \
 	librs_jni \
 	libjni_pinyinime \
-	hostapd_rtl
-
-PRODUCT_PACKAGES += \
-    power.rk2928board \
-    sensors.rk2928board \
-    gralloc.rk2928board \
-    hwcomposer.rk2928board \
-    lights.rk2928board \
-    camera.rk2928board \
-    gpu.rk2928board \
+	hostapd_rtl \
     akmd \
     drmservice \
-    LegacyCamera \
     charger \
-	charger_res_images
-
-# audio lib
-PRODUCT_PACKAGES += \
-    audio_policy.rk2928board \
-    audio.primary.rk2928board
-
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
-    
-PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/proprietary/system/etc/vold.fstab:system/etc/vold.fstab     
-    
-# EXT4 Support
-PRODUCT_PACKAGES += \
+	charger_res_images\
 	make_ext4fs \
 	e2fsck \
 	mkdisfs \
@@ -107,10 +83,26 @@ PRODUCT_PACKAGES += \
 	tune2fs \
 	resize2fs
 
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mass_storage
-	
+# device libs
+PRODUCT_PACKAGES += \
+    audio_policy.rk2928board \
+    audio.primary.rk2928board \
+    power.rk2928board \
+    sensors.rk2928board \
+    gralloc.rk2928board \
+    hwcomposer.rk2928board \
+    lights.rk2928board \
+    camera.rk2928board \
+    gpu.rk2928board
+
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+    
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/proprietary/system/etc/vold.fstab:system/etc/vold.fstab     
+    
+PRODUCT_TAGS += dalvik.gc.type-precise
+
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 
 PRODUCT_NAME := full_rkr70sdk
